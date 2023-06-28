d9937725457e014c1aee0c2676337089badda27f
KegsZooL <114305696+KegsZooL@users.noreply.github.com>
2023-06-28 09:46:19 +0300

    main.c

--- main.c
+++ main.c
@@ -1,21 +1,34 @@
 #define _CRT_SECURE_NO_WARNINGS
 
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 #include "file_manager.h"
 
 int main()
-{
-	char** test = data_input();
+{	
+	system("chcp 1251");
+	system("cls");
+		
+	char** data = data_input();
 
-	for (int i = 0; i < strlen(test); i++)
+	if(data != NULL)
 	{
-		if (strcmp(test[i], "true") == 0)
-			data_output(1);
-		else
-			data_output(0);
-		free(test[i]);
+		for (int i = 0; i < strlen(data); i++)
+		{
+			if (strcmp(data[i], "true") == 0)
+				data_output(1);
+			else
+				data_output(0);
+			printf("%s ", data[i]);
+			free(data[i]);
+		}
 	}
-
-	free(test);
+	else 
+	{	
+		printf("Ошибка чтения файла!\n");
+		return 0;
+	}
+	free(data);
 	return 0;
 }
