2dfe90d964619ca11b3b4265046fc96b968370c8
KegsZooL <114305696+KegsZooL@users.noreply.github.com>
2023-06-29 20:36:07 +0300

    main.c

--- main.c
+++ main.c
@@ -6,30 +6,26 @@
 #include "file_manager.h"
 #include "transliteration.h"
 
-
 int main()
 {	
+	//Изменение кодировки
 	system("chcp 1251");
 	system("cls");
 
-	char** data = data_input();
+	char* data = data_input();
 
 	if(data != NULL)
-	{
-		for (int i = 0; i < strlen(data); i++)
-		{
-			
-			char** result = trl(data[i]);
+	{	
+		char** lexemes = trl(data);
 
-			free(data[i]);
-		}
+		for (int i = 0; i < strlen(data); i++)
+			printf("%c - %s\n", lexemes[i][0], lexemes[i] + 1);
 	}
 	else 
 	{	
 		printf("Ошибка чтения файла!\n");
 		return 0;
 	}
-
 	free(data);
 	return 0;
 }
