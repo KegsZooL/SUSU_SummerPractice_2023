08488f29e157b39702afd55d14cfd668fe056cd7
KegsZooL <114305696+KegsZooL@users.noreply.github.com>
2023-07-03 22:37:55 +0300

    main.py

--- main.py
+++ main.py
@@ -2,7 +2,10 @@
 from file_manager import*
 from transliteration import tansliteration as trl
 from lexerBlock import*
+from identifical import*
 
 string = data_input()
 lexems = trl(string)
 words = Lexer(lexems)
+
+x = identification(words)
