b0726c16d7b080171c4d79664ad3359dda98c983
KegsZooL <114305696+KegsZooL@users.noreply.github.com>
2023-07-04 16:55:58 +0300

    syntax.py

--- syntax.py
+++ syntax.py
@@ -1,7 +1,7 @@
 def Syntax(string):
 
     status = "0"
-    classes = ["hexadecimal", "number", "decimalNum", "str", "bool", "minus"]
+    classes = ["hexadecimal", "number", "decimalNum", "str", "bool"]
 
     for i in range(len(string)):
         print(status)
@@ -20,40 +20,42 @@ def Syntax(string):
                 status = "equal"
             else:
                 return 0
+        elif string[i] == "минус":
+            if status == "equal" or (status == "decimalNum" or status == "number"):
+                status = "minus"
+            else:
+                return 0
+        elif string[i] == "плюс":
+            if status == "decimalNum" or status == "number":
+                status = "plus"
+            else:
+                return 0
+        elif string[i] == "ЦЕЛОЕ":
+            if status == "плюс" or status == "minus" or status == "equal" or status == "dollar":
+                status = "number"
+            else:
+                return 0
         elif string[i] == "доллар":
             if status == "equal":
                 status = "dollar"
             else:
                 return 0
-        elif string[i] == "ЦЕЛОЕ":
-            if status == "dollar":
-                status = "hexadecimal"
-            elif status == "equal":
-                status = "number"
-            else:
-                return 0
-        elif string[i] == "ВЕЩЕСТВЕННАЯ КОНСТАНТА":
-            if status == "equal":
-                status = "decimalNum"
-            else:
-                return 0
+        #Не уверен, что могу складывать строки для своего варианта /-_-|
         elif string[i] == "СТРОКОВАЯ КОНСТАНТА":
             if status == "equal":
                 status = "str"
             else:
                 return 0
-        elif string[i] == "ЛОГКОНСТ_TRUE":
-            if status == "equal":
-                status = "bool"
-            else:
-                return 0
-        elif string[i] == "минус":
-            if status == "equal" and string[i + 1] == "ВЕЩЕСТВЕННАЯ КОНСТАНТА":
-                status = "minus"
-            else:
-                return 0
+
+        # elif string[i] == "ВЕЩЕСТВЕННАЯ КОНСТАНТА":
+        #     if status == ""
+        # elif string[i] == "ЛОГКОНСТ_TRUE":
+        #     if status == "equal":
+        #         status = "bool"
+        #     else:
+        #         return 0
         elif string[i] == "тчкзпт":
-            if status in classes:
+            if status in classes: #переделать
                 status = "end"
             else:
                 return 0
