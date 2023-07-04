4fc6ca561d41a72c69b2b44fe09744cac00c403c
KegsZooL <114305696+KegsZooL@users.noreply.github.com>
2023-07-04 14:58:58 +0300

    main.py

--- main.py
+++ main.py
@@ -3,9 +3,11 @@ from file_manager import*
 from transliteration import tansliteration as trl
 from lexerBlock import*
 from identifical import*
+from syntax import*
 
 string = data_input()
 lexems = trl(string)
 words = Lexer(lexems)
-
-x = identification(words)
+string = identification(words)
+result = Syntax(string)
+data_output(result)
