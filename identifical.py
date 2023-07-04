b637d18f95d3254abac81624b0c90ca2701fcfc8
KegsZooL <114305696+KegsZooL@users.noreply.github.com>
2023-07-04 08:39:05 +0300

    identifical.py

--- identifical.py
+++ identifical.py
@@ -2,9 +2,9 @@ from file_manager import*
 def identification(words):
 
     arr = []
-
-    keyWords = "const"
-    banWords = ["and", "array", "as", "begin", "break",
+    keyWord =  "const"
+    boolConst = ["true", "false"]
+    pascaleWords = ["and", "array", "as", "begin", "break",
                 "case", "class", "xor", "constructor", "continue",
                 "destructor", "div", "do", "downto", "else",
                 "end", "exit", "external", "externalsync", "file",
@@ -13,19 +13,21 @@ def identification(words):
                 "not", "of", "or", "private", "procedure",
                 "program", "property", "protected", "public", "record",
                 "repeat", "set", "shl", "shr", "sizeof",
-                "string", "then", "to", "type", "unit",
-                "until", "uses", "var", "while", "with", "with"]
+                "string", "then", "to", "type", "unit", "until", "uses", "var",
+                "while", "with"]
 
     for i in words:
-        if i[0].lower() == "ИДЕНТИФИКАТОР":
-            if i in banWords:
-                arr.append("ОШИБКА")
-                print("\nНЕДОПУСТИМЫЙ СИМВОЛ")
-
-                data_output(0)
-                exit(0)
-        elif i[0].lower() == keyWords:
+        if i[0].lower() in pascaleWords:
+            
+            arr.append("ОШИБКА")
+            print("\nНЕДОПУСТИМОЕ НАЗВАНИЕ ПЕРЕМЕННОЙ")
+            data_output(0)
+            exit(0)
+            
+        elif i[0].lower() == keyWord:
             arr.append("КЛСЛОВО_" + i[0].upper())
+        elif i[0].lower() in boolConst:
+            arr.append("ЛОГКОНСТ_" + i[0].upper())
         else:
             arr.append(i[1])
     return arr
