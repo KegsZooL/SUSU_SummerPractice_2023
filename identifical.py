04e3fcd77eb4c2694a7a88178465973d463791a4
KegsZooL <114305696+KegsZooL@users.noreply.github.com>
2023-07-05 09:51:01 +0300

    identifical.py

--- identifical.py
+++ identifical.py
@@ -1,32 +1,29 @@
-from file_manager import*
 def identification(words):
 
     arr = []
-    keyWord =  "const"
+    keyWord = "const"
     boolConst = ["true", "false"]
-    pascaleWords = ["and", "array", "as", "begin", "break",
-                "case", "class", "xor", "constructor", "continue",
-                "destructor", "div", "do", "downto", "else",
-                "end", "exit", "external", "externalsync", "file",
-                "finalization", "for", "forward", "function", "if",
-                "in", "inherited", "intialization", "is", "mod",
-                "not", "of", "or", "private", "procedure",
-                "program", "property", "protected", "public", "record",
-                "repeat", "set", "shl", "shr", "sizeof",
-                "string", "then", "to", "type", "unit", "until", "uses", "var",
-                "while", "with"]
+    pascaleWords = ["procedure", "program", "record", "reintroduce",
+                    "function", "goto", "if", "implementation",
+                    "constructor", "destructor", "div", "do",
+                    "in", "inherited", "inline", "interface",
+                    "unit", "until", "uses", "var", "while",
+                    "and", "asm", "array", "begin", "case",
+                    "downto", "else", "end", "file", "for",
+                    "shr", "string", "then", "to", "type",
+                    "object", "of", "or", "packed",
+                    "repeat", "self", "set", "shl",
+                    "label", "mod", "nil", "not",
+                    "with", "xor"]
 
     for i in words:
         if i[0].lower() in pascaleWords:
-
-            arr.append("ОШИБКА")
-            print("\nНЕДОПУСТИМОЕ НАЗВАНИЕ ПЕРЕМЕННОЙ")
-            data_output(0)
-            exit(0)
+            arr.append((i[0], "ПАСКАЛЬ_" + i[0].upper()))
+            print(f"\nНедопустимое название переменной: {i[0].lower()}")
         elif i[0].lower() == keyWord:
-            arr.append("КЛСЛОВО_" + i[0].upper())
+            arr.append((i[0], "КЛСЛОВО_" + i[0].upper()))
         elif i[0].lower() in boolConst:
-            arr.append("ЛОГКОНСТ_" + i[0].upper())
+            arr.append(("ЛОГКОНСТ_" + i[0].upper()))
         else:
-            arr.append(i[1])
+            arr.append((i[0], i[1]))
     return arr
