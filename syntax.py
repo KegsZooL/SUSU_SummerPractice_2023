c7db26e24deea69b4560f98270622c82ddd99632
KegsZooL <114305696+KegsZooL@users.noreply.github.com>
2023-07-06 15:57:22 +0300

    syntax.py

--- syntax.py
+++ syntax.py
@@ -1,10 +1,9 @@
 def Syntax(string):
-
     status = "0"
-    classes = ["number", "decimalNum", "str"]
-    znak = ["minus", "plus"]
+    classes = ["number", "decimalNum", "str", "hexNum", "znak"]
 
     for i in range(len(string)):
+        print(status)
         if "КЛСЛОВО_" in string[i][1]:
             if status == "0":
                 status = "keyword"
@@ -20,19 +19,19 @@ def Syntax(string):
                 status = "equal"
             else:
                 return 0
-        elif string[i][1] == "минус":
+        elif string[i][1] == "знак":
             if status == "equal" or (status == "decimalNum" or status == "number"):
-                status = "minus"
-            else:
-                return 0
-        elif string[i][1] == "плюс":
-            if status == "decimalNum" or status == "number":
-                status = "plus"
+                status = "znak"
             else:
                 return 0
         elif string[i][1] == "ЦЕЛОЕ":
-            if status in znak or status == "equal" or status == "dollar":
+            if status == "equal":
                 status = "number"
+            else:
+                return 0
+        elif string[i][1] == "16-РИЧ":
+            if status == "dollar":
+                status = "hexNum"
             else:
                 return 0
         elif string[i][1] == "доллар":
@@ -40,14 +39,13 @@ def Syntax(string):
                 status = "dollar"
             else:
                 return 0
-        #Не уверен, что не могу складывать строки для своего варианта /-_-|
         elif string[i][1] == "СТРКОНСТ":
             if status == "equal":
                 status = "str"
             else:
                 return 0
         elif string[i][1] == "ВЕЩКОНСТ":
-            if status == "equal" or status in znak:
+            if status == "equal" or status == "znak":
                 status = "decimalNum"
             else:
                 return 0
@@ -56,6 +54,6 @@ def Syntax(string):
                 status = "end"
             else:
                 return 0
-
     if status == "end":
         return 1
+    return 0
